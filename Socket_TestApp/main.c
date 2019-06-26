a02b270da16decd1252144b38be81cad77199610
lexa <voldemar11232@mail.com>
2019-06-26 16:46:15 +0300

    works on local machine (?)

--- Socket_TestApp/main.c
+++ Socket_TestApp/main.c
@@ -9,9 +9,19 @@
 
 int main()
 {
+	WSADATA wsaData;
+
+	int wsaStartupStatus = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaStartupStatus == STATUS_FAILED)
+	{
+		printf("Failed to initialize WSA.\n");
+		getchar();
+		return -1;
+	}
+
 	char data[MESSAGE_SIZE] = "Hello world!";
 
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (serverSocket == INVALID_SOCKET)
 	{
 		printf("Failed to create a socket.\n");
@@ -19,32 +29,44 @@ int main()
 		return -1;
 	}
 
+	char sAddress[64];
+	printf("Enter server IP: ");
+	scanf_s("%s", sAddress, 64);
+
 	struct sockaddr_in serverAddress;
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(DEFAULT_PORT);
-	serverAddress.sin_addr.S_un.S_addr = INADDR_ANY;
+	serverAddress.sin_addr.S_un.S_addr = inet_addr(sAddress);
 
+	printf("Binding...\n");
 	int bindStatus = bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 	if (bindStatus == STATUS_FAILED)
 	{
-		printf("Failed to bind a server.\n");
+		printf("Failed to bind a server: %d\n", WSAGetLastError());
+		getchar();
 		getchar();
 		return -1;
 	}
+	printf("Successfully started a server.\n");
 
-	while (getchar() != 'q')
+	listen(serverSocket, 4);
+
+	SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+	if (clientSocket != INVALID_SOCKET)
 	{
-		listen(serverSocket, 4);
-
-		SOCKET clientSocket = accept(serverSocket, NULL, NULL);
-		if (clientSocket)
+		char message[MESSAGE_SIZE];
+		while (1)
 		{
-			printf("Sent data to client: %ui\n", clientSocket);
-			send(clientSocket, data, MESSAGE_SIZE, 0);
+			scanf_s("%s", message, MESSAGE_SIZE);
+			if (!strcmp(message, "quit")) break;
+			send(clientSocket, message, MESSAGE_SIZE, 0);
+			printf("Sent data to client: \"%s\"\n", message);
 		}
 	}
 
 	closesocket(serverSocket);
+
+	WSACleanup();
 
 	return 0;
 }
--- Socket_TestApp_Client/main.c
+++ Socket_TestApp_Client/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 #include <winsock.h>
 
 #define DEFAULT_PORT 9002
@@ -6,40 +7,69 @@
 #define STATUS_SUCCESS 0
 #define STATUS_FAILED -1
 
+void printCurrentError()
+{
+	char buffer[128];
+	strerror_s(buffer, 128, errno);
+	printf("%s\n", buffer);
+}
+
 int main()
 {
-	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	WSADATA wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	printf("socket: %u\n", clientSocket);
 	if (clientSocket == INVALID_SOCKET)
 	{
 		printf("Failed to create a socket.\n");
+		printCurrentError();
+		getchar();
 		getchar();
 		return -1;
 	}
 
-	struct sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(DEFAULT_PORT);
-	serverAddress.sin_addr.S_un.S_addr = INADDR_ANY;
-
-	//djhaoseifda
-	//can't connect here, actually, idk what am i doing...
-	int connectStatus = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-	if (connectStatus == STATUS_FAILED)
+	while (1)
 	{
-		printf("Failed to connect to a server.\n");
-		getchar();
-		return -1;
+		char sAddress[64];
+		printf("Enter server IP: ");
+		scanf_s("%s", sAddress, 64);
+
+		struct sockaddr_in serverAddress;
+		serverAddress.sin_family = AF_INET;
+		serverAddress.sin_port = htons(DEFAULT_PORT);
+		serverAddress.sin_addr.S_un.S_addr = inet_addr(sAddress);
+
+		printf("Connecting...\n");
+		int connectStatus = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+		if (connectStatus == STATUS_FAILED)
+		{
+			printf("Failed to connect to a server.\n");
+			continue;
+		}
+		break;
 	}
-	//sefklrukgkse
+
+	printf("Connected!\n");
 	char serverResponse[MESSAGE_SIZE] = "-";
-	
-	while (getchar() != 'q')
+	while (1)
 	{
+		if (_kbhit())
+		{
+			if (_getch() == 'q') break;
+		}
+
 		int receiveStatus = recv(clientSocket, serverResponse, MESSAGE_SIZE, 0);
-		if (receiveStatus != STATUS_FAILED) printf("Server response: %s\n", serverResponse);
+		if (receiveStatus != STATUS_FAILED)
+		{
+			printf("Server sent some data: %s\n", serverResponse);
+		}
 	}
 
 	closesocket(clientSocket);
+	WSACleanup();
 
 	return 0;
 }
